f742bb7c5c3bb3eaf3abc7e684e02b1c8043a530
Marcel K. <marcel@aufentha.lt>
2026-04-18 11:05:51 +0200

    now with path option

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use anyhow::Result;
+use std::path::PathBuf;
+
+use anyhow::{Context, Result};
 use rusqlite::Connection;
 use std::fs;
 
 use archivar::archive_top_dir::archive_top_dir;
 
-fn find_directory(name: &str) -> Result<Option<std::path::PathBuf>> {
-    for entry in fs::read_dir(".")? {
+fn find_directory(p :&PathBuf, name: &str) -> Result<Option<std::path::PathBuf>> {
+    for entry in fs::read_dir(p)? {
         let entry = entry?;
         if entry.file_type()?.is_dir() && entry.file_name().to_string_lossy() == name {
             return Ok(Some(entry.path()));
@@ -34,11 +36,27 @@ fn main() -> Result<()> {
                 .action(clap::ArgAction::SetTrue)
                 .help("Remove empty directories"),
         )
+        .arg(
+            clap::Arg::new("path")
+                .short('p')
+                .long("path")
+                .action(clap::ArgAction::Append)
+                .value_name("PATH")
+                .help("Path to the directory to archive (defaults to current directory)")
+        )
         .get_matches();
     let d = matches.get_flag("dry-run");
     let r = matches.get_flag("remove-empty-dirs");
+    let p = match matches.get_one::<String>("path") {
+        Some(path) => PathBuf::from(path),
+        None => std::env::current_dir().context("failed to determine current directory")?,
+    };
 
-    let conn = Connection::open("archivar.db")?;
+    if !p.is_dir() {
+        anyhow::bail!("Provided path '{}' has to be a directory, but it does'nt seem to be one", p.display());
+    }
+
+    let conn = Connection::open(p.join("archivar.db"))?;
     conn.execute(
         "CREATE TABLE IF NOT EXISTS archive (
             year INTEGER NOT NULL,
@@ -51,13 +69,13 @@ fn main() -> Result<()> {
         (),
     )?;
 
-    if let Some(kanzlei_dir) = find_directory("kanzlei")? {
+    if let Some(kanzlei_dir) = find_directory(&p, "kanzlei")? {
         archive_top_dir(kanzlei_dir, d, r, &conn)?;
     } else {
         println!("Directory 'kanzlei' not found, skipping.");
     }
 
-    if let Some(ablage_dir) = find_directory("ablage")? {
+    if let Some(ablage_dir) = find_directory(&p, "ablage")? {
         for bucket in ["2", "4", "6", "8"] {
             let bucket_dir = ablage_dir.join(bucket);
             if bucket_dir.is_dir() {
